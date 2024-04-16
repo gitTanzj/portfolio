1c440653380fbe93fc16c78c24e3db14d1d881c5
gitTanzj <kalle.riit@gmail.com>
2024-04-16 13:56:37 +0300

    testing deployment pt3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,7 +20,7 @@
 
 
 <div class="container">
-    {#if currentPage.route.id !== '/'}
+    {#if currentPage.route.id !== base}
         <div class="navigate-back">
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
             <a href={base}>
